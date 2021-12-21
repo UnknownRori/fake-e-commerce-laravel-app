cb0a3548ec0abeca452fb6a4c96c4122c3716213
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-21 12:32:37 +0700

    initial implementation

---
+++ resources/views/components/paginatebutton.blade.php
@@ -0,0 +1,4 @@
+<div class="row">
+    <a href="{{ $prev }}" disabled class="btn btn-primary mr-2 ml-2 {{ $prev == '' ? 'disabled' : '' }}">Back</a>
+    <a href="{{ $next }}" class="btn btn-primary mr-2 ml-2 {{ $next == '' ? 'disabled' : '' }}">Next</a>
+</div>
