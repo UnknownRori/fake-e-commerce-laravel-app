ea0d1a9627767ff0fb1046715da96963564a5b9f
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-21 18:10:33 +0700

    initial dummy page

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="title">{{ $blog->title }}</x-slot>
+
+    <x-slot name="content">Dummy Text</x-slot>
+</x-layout>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name="title">{{ $product->productname }}</x-slot>
+
+    <x-slot name="content">Dummy Text</x-slot>
+</x-layout>
