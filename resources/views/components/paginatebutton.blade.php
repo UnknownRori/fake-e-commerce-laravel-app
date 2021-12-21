b56cdd14fe78ad3c370ebe93024542e98b33ae54
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-21 12:48:09 +0700

    centering the button

--- resources/views/components/paginatebutton.blade.php
+++ resources/views/components/paginatebutton.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row" style="justify-content: center">
     <a href="{{ $prev }}" disabled class="btn btn-primary mr-2 ml-2 {{ $prev == '' ? 'disabled' : '' }}">Back</a>
     <a href="{{ $next }}" class="btn btn-primary mr-2 ml-2 {{ $next == '' ? 'disabled' : '' }}">Next</a>
 </div>
