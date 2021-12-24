75b57d958ecf82192f01e66c3f3afabd5c04f1da
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-24 09:31:02 +0700

    creating dashboard component

---
+++ resources/views/components/dashboard.blade.php
@@ -0,0 +1,48 @@
+<x-layout>
+    <x-slot name="title">{{ $title }}</x-slot>
+
+    <x-slot name="navbar">
+        @if (Auth::user()->admin)
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Manage Blog
+                </a>
+                <div class="dropdown-menu popout">
+                    <a href="" class="dropdown-item {{ $title == "List owned Blog" ? "active" : "" }}">List owned Blog</a>
+                    <a href="" class="dropdown-item {{ $title == "Create Blog" ? "active" : "" }}">Create Blog</a>
+                    @if (Auth::user()->admin)
+                        <a href="" class="dropdown-item {{ $title == "List all Blog" ? "active" : "" }}">List all Blog</a>
+                    @endif
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Admin
+                </a>
+                <div class="dropdown-menu popout">
+                    <a href="" class="dropdown-item {{ $title == "Users Management" ? "active" : "" }}">Users Management</a>
+                    <a href="" class="dropdown-item {{ $title == "Subscribe Management" ? "active" : "" }}">Subscribe Management</a>
+                    <a href="" class="dropdown-item {{ $title == "Upload Image" ? "active" : "" }}">Upload Image</a>
+                    <a href="" class="dropdown-item {{ $title == "Image Management" ? "active" : "" }}">Image Management</a>
+                    <a href="" class="dropdown-item {{ $title == "Create User" ? "active" : "" }}">Create User</a>
+                </div>
+            </li>
+        @endif
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle {{ !Auth::user()->vendor ? "disabled" : "" }}" href="#" id="navbardrop" data-toggle="dropdown">
+                    Manage Product
+                </a>
+                <div class="dropdown-menu popout">
+                    <a href="" class="dropdown-item {{ $title == "List owned Product" ? "active" : "" }}">List owned Product</a>
+                    <a href="" class="dropdown-item {{ $title == "Create Product" ? "active" : "" }}">Create Product</a>
+                    @if (Auth::user()->admin)
+                        <a href="" class="dropdown-item {{ $title == "List all Product" ? "active" : "" }}">List all Product</a>
+                    @endif
+                </div>
+            </li>
+    </x-slot>
+
+    <x-slot name="content">
+        {{ $content }}
+    </x-slot>
+</x-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,48 +1,7 @@
-<x-layout>
+<x-dashboard>
     <x-slot name="title">Dashboard</x-slot>
-
-    <x-slot name="navbar">
-        @if (Auth::user()->admin)
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                    Manage Blog
-                </a>
-                <div class="dropdown-menu popout">
-                    <a href="" class="dropdown-item">List owned Blog</a>
-                    <a href="" class="dropdown-item">Create Blog</a>
-                    @if (Auth::user()->admin)
-                        <a href="" class="dropdown-item">List all Blog</a>
-                    @endif
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                    Admin
-                </a>
-                <div class="dropdown-menu popout">
-                    <a href="" class="dropdown-item">Users Management</a>
-                    <a href="" class="dropdown-item">Subscribe Management</a>
-                    <a href="" class="dropdown-item">Upload Image</a>
-                    <a href="" class="dropdown-item">Image Management</a>
-                    <a href="" class="dropdown-item">Create Users</a>
-                </div>
-            </li>
-        @endif
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle {{ !Auth::user()->vendor ? "disabled" : "" }}" href="#" id="navbardrop" data-toggle="dropdown">
-                    Manage Product
-                </a>
-                <div class="dropdown-menu popout">
-                    <a href="" class="dropdown-item">List owned Product</a>
-                    <a href="" class="dropdown-item">Create Product</a>
-                    @if (Auth::user()->admin)
-                        <a href="" class="dropdown-item">List all Product</a>
-                    @endif
-                </div>
-            </li>
-    </x-slot>
 
     <x-slot name="content">
 
     </x-slot>
-</x-layout>
+</x-dashboard>
