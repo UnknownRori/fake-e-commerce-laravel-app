ce7993f9d1ef3b99f859941e43bfcd38eec80ab9
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-24 09:22:14 +0700

    implementing subscribe feature

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subscribe;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SubscribeController extends Controller
 {
-    //
 
-    public function Subscribe (Request $request) {
+    public function Subscribe (Request $request)
+    {
+
+        $validate = $request->validate([
+            'email' => 'required|string|unique:subscribe,email'
+        ]);
+
+        if ($validate['email'] == Auth::user()->email) {
+
+            $Subscribe = new Subscribe();
+            $Subscribe->users_id = Auth::user()->id;
+            $Subscribe->email = $validate['email'];
+
+            if ($Subscribe->save()) {
+                session()->flash('success', 'Your account successfully registered to our newsletter!');
+            }
+
+            return redirect()->route("Home");
+        } else {
+            session()->flash('fail', 'You probably should use that email already registered in this!');
+            return redirect()->route("Home");
+        }
 
     }
 
