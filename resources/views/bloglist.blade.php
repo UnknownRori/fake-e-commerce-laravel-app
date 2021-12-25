4a99deb8bab8a2cc6ef3e5aa95a833546b1d6a8e
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-25 08:23:02 +0700

    revise display

--- resources/views/bloglist.blade.php
+++ resources/views/bloglist.blade.php
@@ -3,27 +3,57 @@
 
     <x-slot name="content">
         <main>
-            <header class="blog-welcome">
-                <h2>Blog</h2>
-            </header>
+            <section class="blog-welcome text-center">
+                <header class="" style="padding-top: 10rem;">
+                    <img class="" style="width: 150px;" src="{{ asset("image/Apple.png") }}" alt="">
+                    <h2 class="text-center" id="blog_introduction"></h2>
+                </header>
+            </section>
 
             <div class="container">
                 <article>
                     <table>
                         @foreach ($blog as $row)
-                        <tr data-hidden="{{ $row->id }}">
+                        <tr data-hidden="{{ $row->id }}" class="hidden">
                             <td rowspan="2" class="img-container">
                                 <a href="{{ route("Blog", $row->id) }}">
                                     <img class="img-fluid" src="{{ asset("image/blog/" . $row->title) . ".png" }}" alt="{{ $row->title }}">
                                 </a>
                             </td>
                             <td>
-                                <a href="{{ route("Product", $row->id) }}">
-                                    <h3 class="ml-1 text-black">{{ $row->title }}</h3>
-                                </a>
+                                <div class="row">
+                                    <div class="col-8">
+                                         <a href="{{ route("Product", $row->id) }}">
+                                            <h3 class="ml-1 text-black">{{ $row->title }}</h3>
+                                        </a>
+                                    </div>
+                                    <div class="col-2">
+                                        @if(Auth::user()->admin)
+                                            <div class="float-right dropdown">
+                                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    Action
+                                                </button>
+                                                <div class="dropdown-menu popout" aria-labelledby="dropdownMenuButton" style="margin-right: 2rem;">
+                                                    @if (Auth::user()->id == $row->users_id)
+                                                        <a href="#" class="dropdown-item">Edit</a>
+                                                        <form action="" method="post">
+                                                            @csrf
+                                                            @method('delete')
+                                                            <input type="submit" value="Delete" class="dropdown-item">
+                                                        </form>
+                                                    @endif
+
+                                                    @if (Auth::user()->id != $row->users_id)
+                                                        <a class="dropdown-item" href="#">Report</a>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
                             </td>
                         </tr>
-                        <tr data-hidden="{{ $row->id }}">
+                        <tr data-hidden="{{ $row->id }}" class="hidden">
                             <td>
                                 <p class="ml-1">
                                     {{ $row->content }}
