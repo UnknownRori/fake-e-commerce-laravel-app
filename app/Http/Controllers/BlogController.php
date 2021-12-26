80c48cd39c60d95e897d208fbaa3082e65d73f82
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-26 13:18:59 +0700

    implement crud

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,6 +6,8 @@ use App\Models\Blog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class BlogController extends Controller
 {
@@ -32,15 +34,79 @@ class BlogController extends Controller
         return view('dashboard.blogform');
     }
     public function Create (Request $request) {
-        dd($request);
+        $validate = $request->validate([
+            'title' => 'required|string|unique:blog,title',
+            'content' => 'required|string',
+            'photo' => 'required|image'
+        ]);
+
+        $blog = new Blog();
+        $blog->users_id = Auth::user()->id;
+        $blog->title = $validate['title'];
+        $blog->content = $validate['content'];
+
+        if($blog->save()) {
+
+            if (BlogController::UploadPhoto($validate['photo'], $validate['title'], null)) {
+                session()->flash('success', 'Blog successfully posted!');
+                return redirect()->back();
+            }
+            session()->flash('fail', 'Image cannot be saved! but Blog has been posted!');
+            return redirect()->back();
+        }
+
+        session()->flash('fail', 'Internal Server Error!');
+        return redirect()->back();
+
     }
 
     public function Update (Request $request) {
-        dd($request);
+        $validate = $request->validate([
+            'title' => 'required|string|unique:blog,title',
+            'content' => 'required|string',
+            'photo' => 'required|image'
+        ]);
+
+        $blog = Blog::find($request->id);
+        $oldphoto = $blog->title;
+        $blog->title = $validate['title'];
+        $blog->content = $validate['content'];
+
+        if ($blog->save()) {
+
+            if (BlogController::UploadPhoto($validate['photo'], $validate['title'], $oldphoto)) {
+                session()->flash('success', 'Blog successfully edited!');
+                return redirect()->back();
+            }
+            session()->flash('fail', 'Image cannot be saved! but Blog has been edited!');
+            return redirect()->back();
+        }
+
+        session()->flash('fail', 'Internal Server Error!');
+        return redirect()->back();
     }
 
     public function Delete (Request $request) {
-        dd($request);
+        $blog = Blog::find($request->id);
+        if($blog == null) {
+            session()->flash('fail', 'Failed to delete blog!');
+            return redirect()->back();
+        }
+
+        if (Auth::user()->id == $blog->users_id || Auth::user()->admin) {
+
+            if (DB::table('blog')->where('id', $blog->id)->delete()) {
+                session()->flash('success', 'Blog successfully deleted!');
+                return redirect()->back();
+            }
+
+            session()->flash('fail', 'Failed to delete blog!');
+            return redirect()->back();
+        } else {
+            session()->flash('fail', 'Invalid Pervilege!');
+            return redirect()->route('Home');
+        }
+
     }
 
     public function AllBlogList () {
@@ -83,5 +149,31 @@ class BlogController extends Controller
         return view('blog', [
             'Blog' => $Blog
         ]);
+    }
+
+    protected static function UploadPhoto($photo, $title, $oldphoto) {
+        $directory = "public/image/blog";
+
+        if (!$oldphoto || $oldphoto == "") {
+            dd("acat");
+            $path = Storage::putFileAs(
+                $directory,
+                $photo,
+                $title . '.png'
+            );
+            return $path;
+        } else {
+            if (!Storage::delete($directory . '/' . $oldphoto . '.png')) {
+                session()->flash('fail', 'Old image failed to deleted!');
+            }
+
+            $path = Storage::putFileAs(
+                $directory,
+                $photo,
+                $title . '.png'
+            );
+
+            return $path;
+        }
     }
 }
