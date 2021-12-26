980ca8c9ddae65b0ca90a3d74bd6e9006affe69f
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-26 13:06:56 +0700

    fixing action form

--- resources/views/dashboard/blogform.blade.php
+++ resources/views/dashboard/blogform.blade.php
@@ -3,19 +3,17 @@
 
     <x-slot name="content">
         <form class="bg-light" style="border: 0.1px solid gray; padding: 2rem; box-shadow: 2px 2px 2px 2px gray;"
-         action="{{ route("CreateBlog") }}" method="post" enctype="multipart/form-data">
+         action="{{ isset($blog) ? route("EditBlog", $blog->id) : route("CreateBlog") }}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
                 <input class="form-control" type="text" name="title" placeholder="Blog Title"
                 value="{{ isset($blog) ? $blog->title : "" }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="file" name="Photo" placeholder="Blog Photo">
+                <input class="form-control" type="file" name="photo" placeholder="Blog Photo">
             </div>
             <div class="form-group">
-                <textarea class="form-control" name="content" id="" cols="30" rows="10" placeholder="Blog Content">
-                    {{ isset($blog) ? $blog->content : "" }}
-                </textarea>
+                <textarea class="form-control" name="content" id="" cols="30" rows="10" placeholder="Blog Content">{{ isset($blog) ? $blog->content : "" }}</textarea>
             </div>
             <div class="form-group">
                 <input class="btn btn-info" type="submit" value="Create">
--- resources/views/dashboard/productform.blade.php
+++ resources/views/dashboard/productform.blade.php
@@ -1,16 +1,16 @@
 <x-dashboard>
-    <x-slot name="title">{{ !isset($product) ? "Create Blog" : "Edit Blog " . $product->title }}</x-slot>
+    <x-slot name="title">{{ !isset($product) ? "Create Product" : "Edit Product " . $product->title }}</x-slot>
 
     <x-slot name="content">
         <form class="bg-light" style="border: 0.1px solid gray; padding: 2rem; box-shadow: 2px 2px 2px 2px gray;"
-         action="{{ route("CreateBlog") }}" method="post" enctype="multipart/form-data">
+         action="{{ isset($product) ? route("CreateProduct") : route("CreateProduct") }}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
                 <input class="form-control" type="text" name="productname" placeholder="Product Name"
                 value="{{ isset($product) ? $product->productname : "" }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="file" name="Photo" placeholder="Product Name">
+                <input class="form-control" type="file" name="photo" placeholder="Product Name">
             </div>
             <div class="form-group">
                 <input class="form-control" type="number" name="stock" placeholder="Starting Stock"
@@ -21,9 +21,7 @@
                 value="{{ isset($product) ? $product->price : "" }}">
             </div>
             <div class="form-group">
-                <textarea class="form-control" name="description" id="" cols="30" rows="10" placeholder="Product Description">
-                    {{ isset($product) ? $product->description : "" }}
-                </textarea>
+                <textarea class="form-control" name="description" id="" cols="30" rows="10" placeholder="Product Description">{{ isset($product) ? $product->description : "" }}</textarea>
             </div>
             <div class="form-group">
                 <input class="btn btn-info" type="submit" value="Create">
