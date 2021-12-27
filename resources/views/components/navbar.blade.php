d9e5710c932652257ec5d563cbfbebd9dea8df2f
Akbar-Hendra-Jaya <68576836+UnknownRori@users.noreply.github.com>
2021-12-27 10:17:29 +0700

    add profile picture in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -38,6 +38,7 @@
                 <li class="nav-item dropdown mr-5">
                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                         {{ Auth::user()->username }}
+                        <img class="profile m-0 p-0" style="width: 2rem;" src="{{ Storage::url('/image/profile/' . Auth::user()->username . '.png') }}">
                     </a>
                     <div class="dropdown-menu popout">
                         <a href="{{ route("Dashboard") }}" class="dropdown-item {{ $title == "Dashboard" ? "active" : "" }}">Dashboard</a>
@@ -50,30 +51,5 @@
 
         </ul>
         </div>
-
-        {{-- <ul class="navbar-nav">
-            @if (!Auth::check())
-                <div class="row">
-                    <li class="nav-item mr-2">
-                        <a href="{{ route("Login") }}" class="nav-link {{ $title == "Login" ? "active" : "" }}">Login</a>
-                    </li>
-                    <li class="nav-item ml-2 mr-2">
-                        <a href="{{ route("Register") }}" class="nav-link {{ $title == "Register" ? "active" : "" }}">Register</a>
-                    </li>
-                </div>
-            @else
-                <li class="nav-item dropdown mr-5">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                        {{ Auth::user()->username }}
-                    </a>
-                    <div class="dropdown-menu popout">
-                        <a href="{{ route("Dashboard") }}" class="dropdown-item {{ $title == "Dashboard" ? "active" : "" }}">Dashboard</a>
-                        <a href="" class="dropdown-item">User Setting</a>
-                        <a href="{{ route("Logout") }}" class="dropdown-item">Log out</a>
-                    </div>
-                </li>
-            @endif
-
-        </ul> --}}
     </div>
 </nav>
