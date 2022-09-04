4c3da9efa798adf56bdcf1a894f19ea597d7ebd7
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-05 02:58:27 +0700

    🔨 Remove unused `use`

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Reviews;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class ReviewsController extends Controller
 {
