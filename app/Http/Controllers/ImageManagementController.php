ab9f482766f19d86e05bac533de389c498561d14
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-07 02:35:41 +0700

    🔨 Refactor ImageManagementController

--- app/Http/Controllers/ImageManagementController.php
+++ app/Http/Controllers/ImageManagementController.php
@@ -3,23 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class ImageManagementController extends Controller
 {
-    private static function AdminOnly()
-    {
-        if (!Auth::user()->admin) {
-            session()->flash('fail', 'Invalid Pervilege');
-            return redirect()->back();
-        }
-    }
-
     public function Index()
     {
-        ImageManagementController::AdminOnly();
-
         $files = Storage::allFiles('/public/image');
 
         return view('dashboard.imagemanagement', [
@@ -29,46 +18,33 @@ class ImageManagementController extends Controller
 
     public function Delete(Request $request)
     {
-        ImageManagementController::AdminOnly();
-
-        if (Auth::user()->admin) {
             $validate = $request->validate([
                 'uri' => 'required|string'
             ]);
 
-            if (Storage::delete($validate)) {
-                session()->flash('success', 'Image successfully deleted!');
-                return redirect()->back();
-            }
-            session()->flash('fail', 'Image failed to delete');
-            return redirect()->back();
-        } else {
-            session()->flash('fail', 'Invalid Pervilege');
-            return redirect()->back();
-        }
+            if (Storage::delete($validate))
+                return redirect()->back()->with('success', 'Image successfully deleted!');
+
+            return redirect()->back()->with('fail', 'Image failed to delete');
     }
 
     public function View()
     {
-        ImageManagementController::AdminOnly();
-
         return view('dashboard.imageupload');
     }
 
     public function Create(Request $request)
     {
-        ImageManagementController::AdminOnly();
-
         $validate = $request->validate([
             'path' => 'required|string',
             'image' => 'required|image'
         ]);
+
         $directory = "public/image/" . $validate['path'];
-        if ($request->file('image')->store($directory)) {
-            session()->flash('success', 'Image successfully uploaded');
-            return redirect()->back();
-        }
-        session()->flash('fail', 'Image failed to upload');
-        return redirect()->back();
+
+        if ($request->file('image')->store($directory))
+            return redirect()->back()->with('success', 'Image successfully uploaded');
+
+        return redirect()->back()->with('fail', 'Image failed to upload');
     }
 }
