9bf9a9c1c1f030d28dcc6a66584bd3f84c5efbe5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-08 14:41:24 +0700

    🔨 Implement Request class on Blog Create and Update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BlogCreateRequest;
+use App\Http\Requests\BlogUpdateRequest;
 use App\Models\Blog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -32,58 +34,40 @@ class BlogController extends Controller
 
         return view('dashboard.blogform');
     }
-    public function Create(Request $request)
+    public function Create(BlogCreateRequest $request)
     {
-        $validate = $request->validate([
-            'title' => 'required|string|unique:blog,title',
-            'content' => 'required|string',
-            'photo' => 'required|image'
-        ]);
-
         $blog = new Blog();
         $blog->users_id = Auth::user()->id;
-        $blog->title = $validate['title'];
-        $blog->content = $validate['content'];
+        $blog->title = $request->title;
+        $blog->content = $request->content;
 
         if ($blog->save()) {
 
-            if (BlogController::UploadPhoto($validate['photo'], $validate['title'], null)) {
-                session()->flash('success', 'Blog successfully posted!');
-                return redirect()->back();
-            }
-            session()->flash('fail', 'Image cannot be saved! but Blog has been posted!');
-            return redirect()->back();
+            if (BlogController::UploadPhoto($request->photo, $request->title, null))
+                return redirect()->back()->with('success', 'Blog successfully posted!');
+
+            return redirect()->back()->with('fail', 'Image cannot be saved! but Blog has been posted!');
         }
 
-        session()->flash('fail', 'Internal Server Error!');
-        return redirect()->back();
+        return redirect()->back()->with('fail', 'Internal Server Error!');
     }
 
-    public function Update(Request $request)
+    public function Update(BlogUpdateRequest $request)
     {
-        $validate = $request->validate([
-            'title' => 'required|string|unique:blog,title',
-            'content' => 'required|string',
-            'photo' => 'required|image'
-        ]);
-
         $blog = Blog::find($request->id);
         $oldphoto = $blog->title;
-        $blog->title = $validate['title'];
-        $blog->content = $validate['content'];
+        $blog->title = $request->title;
+        $blog->content = $request->content;
 
         if ($blog->save()) {
 
-            if (BlogController::UploadPhoto($validate['photo'], $validate['title'], $oldphoto)) {
-                session()->flash('success', 'Blog successfully edited!');
-                return redirect()->back();
-            }
-            session()->flash('fail', 'Image cannot be saved! but Blog has been edited!');
-            return redirect()->back();
+            if (BlogController::UploadPhoto($request->photo, $request->title, $oldphoto))
+                return redirect()->back()->with('success', 'Blog successfully edited!');
+
+            return redirect()->back()->with('fail', 'Image cannot be saved! but Blog has been edited!');
         }
 
-        session()->flash('fail', 'Internal Server Error!');
-        return redirect()->back();
+        return redirect()->back()->with('fail', 'Internal Server Error!');
     }
 
     public function Delete(Request $request)
